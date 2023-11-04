984eb12685ee5b2bb567d035fbf4cd1ab277e033
FD <fa.drsh1401@gmail.com>
2023-11-04 22:19:00 +0330

    first version

---
+++ Tower of Hanoi/Tower of Hanoi/Source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void han(int x, char a, char temp, char b)
+{
+	int count = 0;
+	if (x == 1)
+		cout << " number " << x << " from " << a << " to " << b << endl;
+
+	else
+	{
+		han(x - 1, a, b, temp);
+		cout << " number " << x << " from " << a << " to " << b << endl;
+		han(x - 1, temp, a, b);
+	}
+
+}
+void main()
+{
+	int n;
+	char A, B, C;
+	cin >> n;
+	cin >> A >> B >> C;
+	han(n, A, B, C);
+
+}
